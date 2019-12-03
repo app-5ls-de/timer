1de54bade32c6d93f33e9620a325f95c7be78630
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2019-12-03 16:35:39 +0100

    added two buttons for plus and minus

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var h1 = document.getElementsByTagName('time')[0],
 bt_start = document.getElementById('start'),
 bt_stop = document.getElementById('stop'),
 bt_clear = document.getElementById('clear'),
+bt_plus = document.getElementById('plus'),
+bt_minus = document.getElementById('minus'),
 startTime = null,running=false ,duration = moment.duration(0);
 
 vars = {};
@@ -15,6 +17,7 @@ function init(vars) {
   if (moment.isDuration(moment.duration(vars['d'])) & vars['t']==null & vars['r']=="0"){
     running=false;
     duration = moment.duration(vars['d']);
+    if (duration<0){clear(); return}
     show(duration);
     startTime = null;
   }
@@ -24,6 +27,7 @@ function init(vars) {
     duration = null
     update()
   }
+  
 }
 
 show(duration);
@@ -42,12 +46,13 @@ function timer() {
 }
 
 function show(dur) {
+  if (dur<0){clear(); return}
   var seconds = dur.seconds();
   var minutes = dur.minutes();
   var hours = Math.floor(dur.asHours());
   h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 }
-// timer();
+
 
 function start() {
   console.log('start');
@@ -88,6 +93,27 @@ function clear() {
   window.history.replaceState(null, null, newURL);
 }
 
+function addseconds(seconds) {
+  var seconds_duration = moment.duration(seconds, 'seconds')
+  if (!moment.isDuration(seconds_duration)) {return}
+
+  if (startTime!=null) {
+    startTime.subtract(seconds_duration)
+    var newURL = "/?r=1&t=" + startTime.format();
+    window.history.replaceState(null, null, newURL);
+    update()
+    return
+  }
+
+  if (duration!=null) {
+    duration.add(seconds_duration)
+    if (duration<0){clear(); return}
+    var newURL = "/?r=0&d=" + duration.toISOString();
+    window.history.replaceState(null, null, newURL);
+    show(duration)
+  }
+}
+
 
 
 
@@ -104,9 +130,8 @@ function debug() {
 //start();
 
 
-/* Start button */
 bt_start.onclick = start;
-/* Stop button */
 bt_stop.onclick = stop;
-/* Clear button */
 bt_clear.onclick = clear;
+bt_plus.onclick = function () {addseconds(60)};
+bt_minus.onclick = function () {addseconds(-60)};
