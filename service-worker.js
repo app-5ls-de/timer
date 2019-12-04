d2160c584cd72a363fb77fa549443fe962ff3f56
PatrickHaussmann <36867676+PatrickHaussmann@users.noreply.github.com>
2019-12-04 21:53:55 +0100

    tests

---
+++ service-worker.js
@@ -0,0 +1,106 @@
+const CACHE = "static-cache-v2";
+const precacheFiles = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/images/logo16px.png',
+  '/images/logo24px.png',
+  '/images/logo32px.png',
+  '/images/logo64px.png',
+  '/images/logo128px.png',
+  '/images/logo256px.png',
+  '/images/logo512px.png',
+  '/images/android-icon-192x192.png',
+  '/images/favicon-16x16.png',
+  '/images/favicon-32x32.png',
+  '/images/favicon-96x96.png'
+];
+
+
+self.addEventListener("install", function (event) {
+  console.log("[ServiceWorker] Install Event processing");
+
+  console.log("[ServiceWorker] Skip waiting on install");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[ServiceWorker] Caching pages during install");
+      return cache.addAll(precacheFiles);
+    })
+  );
+});
+
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("[ServiceWorker] Claiming clients for current page");
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  event.waitUntil(self.clients.claim());
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) { 
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
+
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
+
+        return response;
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            console.log("[ServiceWorker] Network request failed and no cache." + error);
+          });
+      }
+    )
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
