c622e797436529d994bdcf8bf413567e4a3e15d0
patrickhaussmann <git@patrickhaussmann.de>
2021-08-26 12:44:07 +0200

    Improve Caching

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,9 @@ const { registerRoute, setDefaultHandler } = workbox.routing;
 const { StaleWhileRevalidate, CacheFirst } = workbox.strategies;
 const { cacheNames, setCacheNameDetails } = workbox.core;
 
-setCacheNameDetails({ suffix: "v1" });
+setCacheNameDetails({ suffix: "v2" });
+
+registerRoute(new RegExp("\\.(json|svg|png|woff2?)$"), new CacheFirst());
 
 registerRoute(
   ({ url }) => url.origin == location.origin,
@@ -16,19 +18,6 @@ registerRoute(
   ({ url }) => url.origin == "https://cdn.jsdelivr.net",
   new CacheFirst()
 );
-
-self.addEventListener("install", (event) => {
-  const urls = [
-    "/",
-    "/script.js",
-    "/style.css",
-    "https://cdn.jsdelivr.net/npm/dayjs@1.10.6/dayjs.min.js",
-    "https://cdn.jsdelivr.net/npm/dayjs@1.10.6/plugin/duration.js",
-    "https://cdn.jsdelivr.net/npm/long-press-event@2.4.4/dist/long-press-event.min.js",
-  ];
-  const cacheName = cacheNames.runtime;
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(urls)));
-});
 
 self.addEventListener("activate", (event) => {
   const cacheNamesArray = Object.values(cacheNames);
