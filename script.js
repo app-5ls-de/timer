ae4170729ef5715d778ce46663b36efdac4eb80b
patrickhaussmann <git@patrickhaussmann.de>
2021-12-20 23:42:02 +0100

    check if wakeLock avalable

--- script.js
+++ script.js
@@ -18,13 +18,11 @@ let wakeLock = null;
 // Function that attempts to request a screen wake lock.
 const requestWakeLock = async () => {
   try {
+    if ("wakeLock" in window ) {
     wakeLock = await navigator.wakeLock.request();
-    wakeLock.addEventListener('release', () => {
-      console.log('Screen Wake Lock released:', wakeLock.released);
-    });
-    console.log('Screen Wake Lock released:', wakeLock.released);
+    }
   } catch (err) {
-    console.error(`${err.name}, ${err.message}`);
+    console.error(err.name+", "+err.message);
   }
 };
 
